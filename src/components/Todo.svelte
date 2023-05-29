17d4d9a4edc796c572468185af6ad63689e13843
Archie180 <archie0108.be20@chitkara.edu.in>
2023-05-29 19:07:51 +0530

    completed

---
+++ src/components/Todo.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { deleteTodo ,toggleTodoCompleted} from './../stores/todoStore.js';
+    
+    export let todo;
+  console.log(todo.id)
+
+
+</script>
+<li class=" flex flex-row items-center justify-between mb-3 py-3 px-4 md:text-xl text-md  border border-gray-300">
+<input name="completed" class="mr-2  h-8 w-6 " type="checkbox" checked={todo.completed} on:change={()=>toggleTodoCompleted(todo.id)}/>
+<span class={`flex-1 font-bold ${todo.completed? 'line-through':''}`}>{todo.text}</span>
+<button class="bg-red-600 text-white p-2 rounded-md" on:click={deleteTodo(todo.id)}>Delete</button>
+</li>
---
+++ src/components/Todoform.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { addTodo } from './../stores/todoStore.js';
+
+    let todoval="";
+    const handleSubmit=()=>{
+        console.log(todoval)
+        addTodo(todoval);
+        todoval="";
+        console.log("submitting")
+    }
+</script>
+<form class="my-8" on:submit|preventDefault={handleSubmit}>
+    <div class="flex flex-col gap-3">
+    <label for="todo" class="font-bold text-xl">Todos</label>
+    <input class="px-3 py-2 border text-md rounded-md border-gray-400 focus:outline-none" type="text" bind:value={todoval}  name="todo"  placeholder="Write here"/>
+    </div>
+   
+<button type="submit" class="mt-3 w-full text-md sm:text-xl bg-blue-500 text-white hover:bg-blue-600 py-2 rounded-md" >Submit</button>
+</form>
